4a693b717dd6080d8b26adf95dcd9cea5083b98c
hempflower <1993996310@qq.com>
2019-07-10 22:22:15 +0800

    升级Gradle4.9,并暂时移除了CraftBukkit

---
+++ src/main/java/cn/orecraft/orepowered/OreServer.java
@@ -0,0 +1,553 @@
+/*
+ * Created on 2019/7/10
+ * Author: MaHua_A
+ * Copyright 2019 by OreCraft Studio
+ * DO NOT MODIFY THESE WORDS
+ */
+
+
+package cn.orecraft.orepowered;
+
+import net.minecraft.server.MinecraftServer;
+import org.bukkit.*;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.help.HelpMap;
+import org.bukkit.inventory.*;
+import org.bukkit.map.MapView;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.ServicesManager;
+import org.bukkit.plugin.messaging.Messenger;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.util.CachedServerIcon;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class OreServer implements Server {
+    private final String serverversion = "0.0.1";
+    private final MinecraftServer mcserver;
+    public OreServer(MinecraftServer mcserver){
+        this.mcserver = mcserver;
+    }
+    @Override
+    public String getName() {
+        return "OrePowered";
+    }
+
+    @Override
+    public String getVersion() {
+        return "Minecraft/"+mcserver.getMinecraftVersion();
+    }
+
+    @Override
+    public String getBukkitVersion() {
+        return "unknown";
+    }
+
+    @Override
+    public Collection<? extends Player> getOnlinePlayers() {
+        return null;
+    }
+
+    @Override
+    public int getMaxPlayers() {
+        return this.mcserver.getMaxPlayers();
+    }
+
+    @Override
+    public int getPort() {
+        return this.mcserver.getServerPort();
+    }
+
+    @Override
+    public int getViewDistance() {
+        return this.mcserver.get;
+    }
+
+    @Override
+    public String getIp() {
+        return null;
+    }
+
+    @Override
+    public String getServerName() {
+        return null;
+    }
+
+    @Override
+    public String getServerId() {
+        return null;
+    }
+
+    @Override
+    public String getWorldType() {
+        return null;
+    }
+
+    @Override
+    public boolean getGenerateStructures() {
+        return false;
+    }
+
+    @Override
+    public boolean getAllowEnd() {
+        return false;
+    }
+
+    @Override
+    public boolean getAllowNether() {
+        return false;
+    }
+
+    @Override
+    public boolean hasWhitelist() {
+        return false;
+    }
+
+    @Override
+    public void setWhitelist(boolean value) {
+
+    }
+
+    @Override
+    public Set<OfflinePlayer> getWhitelistedPlayers() {
+        return null;
+    }
+
+    @Override
+    public void reloadWhitelist() {
+
+    }
+
+    @Override
+    public int broadcastMessage(String message) {
+        return 0;
+    }
+
+    @Override
+    public String getUpdateFolder() {
+        return null;
+    }
+
+    @Override
+    public File getUpdateFolderFile() {
+        return null;
+    }
+
+    @Override
+    public long getConnectionThrottle() {
+        return 0;
+    }
+
+    @Override
+    public int getTicksPerAnimalSpawns() {
+        return 0;
+    }
+
+    @Override
+    public int getTicksPerMonsterSpawns() {
+        return 0;
+    }
+
+    @Override
+    public Player getPlayer(String name) {
+        return null;
+    }
+
+    @Override
+    public Player getPlayerExact(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Player> matchPlayer(String name) {
+        return null;
+    }
+
+    @Override
+    public Player getPlayer(UUID id) {
+        return null;
+    }
+
+    @Override
+    public PluginManager getPluginManager() {
+        return null;
+    }
+
+    @Override
+    public BukkitScheduler getScheduler() {
+        return null;
+    }
+
+    @Override
+    public ServicesManager getServicesManager() {
+        return null;
+    }
+
+    @Override
+    public List<World> getWorlds() {
+        return null;
+    }
+
+    @Override
+    public World createWorld(WorldCreator creator) {
+        return null;
+    }
+
+    @Override
+    public boolean unloadWorld(String name, boolean save) {
+        return false;
+    }
+
+    @Override
+    public boolean unloadWorld(World world, boolean save) {
+        return false;
+    }
+
+    @Override
+    public World getWorld(String name) {
+        return null;
+    }
+
+    @Override
+    public World getWorld(UUID uid) {
+        return null;
+    }
+
+    @Override
+    public MapView getMap(short id) {
+        return null;
+    }
+
+    @Override
+    public MapView createMap(World world) {
+        return null;
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
+    @Override
+    public void reloadData() {
+
+    }
+
+    @Override
+    public Logger getLogger() {
+        return null;
+    }
+
+    @Override
+    public PluginCommand getPluginCommand(String name) {
+        return null;
+    }
+
+    @Override
+    public void savePlayers() {
+
+    }
+
+    @Override
+    public boolean dispatchCommand(CommandSender sender, String commandLine) throws CommandException {
+        return false;
+    }
+
+    @Override
+    public boolean addRecipe(Recipe recipe) {
+        return false;
+    }
+
+    @Override
+    public List<Recipe> getRecipesFor(ItemStack result) {
+        return null;
+    }
+
+    @Override
+    public Iterator<Recipe> recipeIterator() {
+        return null;
+    }
+
+    @Override
+    public void clearRecipes() {
+
+    }
+
+    @Override
+    public void resetRecipes() {
+
+    }
+
+    @Override
+    public Map<String, String[]> getCommandAliases() {
+        return null;
+    }
+
+    @Override
+    public int getSpawnRadius() {
+        return 0;
+    }
+
+    @Override
+    public void setSpawnRadius(int value) {
+
+    }
+
+    @Override
+    public boolean getOnlineMode() {
+        return false;
+    }
+
+    @Override
+    public boolean getAllowFlight() {
+        return false;
+    }
+
+    @Override
+    public boolean isHardcore() {
+        return false;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public int broadcast(String message, String permission) {
+        return 0;
+    }
+
+    @Override
+    public OfflinePlayer getOfflinePlayer(String name) {
+        return null;
+    }
+
+    @Override
+    public OfflinePlayer getOfflinePlayer(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getIPBans() {
+        return null;
+    }
+
+    @Override
+    public void banIP(String address) {
+
+    }
+
+    @Override
+    public void unbanIP(String address) {
+
+    }
+
+    @Override
+    public Set<OfflinePlayer> getBannedPlayers() {
+        return null;
+    }
+
+    @Override
+    public BanList getBanList(BanList.Type type) {
+        return null;
+    }
+
+    @Override
+    public Set<OfflinePlayer> getOperators() {
+        return null;
+    }
+
+    @Override
+    public GameMode getDefaultGameMode() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultGameMode(GameMode mode) {
+
+    }
+
+    @Override
+    public ConsoleCommandSender getConsoleSender() {
+        return null;
+    }
+
+    @Override
+    public File getWorldContainer() {
+        return null;
+    }
+
+    @Override
+    public OfflinePlayer[] getOfflinePlayers() {
+        return new OfflinePlayer[0];
+    }
+
+    @Override
+    public Messenger getMessenger() {
+        return null;
+    }
+
+    @Override
+    public HelpMap getHelpMap() {
+        return null;
+    }
+
+    @Override
+    public Inventory createInventory(InventoryHolder owner, InventoryType type) {
+        return null;
+    }
+
+    @Override
+    public Inventory createInventory(InventoryHolder owner, InventoryType type, String title) {
+        return null;
+    }
+
+    @Override
+    public Inventory createInventory(InventoryHolder owner, int size) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Inventory createInventory(InventoryHolder owner, int size, String title) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Merchant createMerchant(String title) {
+        return null;
+    }
+
+    @Override
+    public int getMonsterSpawnLimit() {
+        return 0;
+    }
+
+    @Override
+    public int getAnimalSpawnLimit() {
+        return 0;
+    }
+
+    @Override
+    public int getWaterAnimalSpawnLimit() {
+        return 0;
+    }
+
+    @Override
+    public int getAmbientSpawnLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean isPrimaryThread() {
+        return false;
+    }
+
+    @Override
+    public String getMotd() {
+        return null;
+    }
+
+    @Override
+    public String getShutdownMessage() {
+        return null;
+    }
+
+    @Override
+    public Warning.WarningState getWarningState() {
+        return null;
+    }
+
+    @Override
+    public ItemFactory getItemFactory() {
+        return null;
+    }
+
+    @Override
+    public ScoreboardManager getScoreboardManager() {
+        return null;
+    }
+
+    @Override
+    public CachedServerIcon getServerIcon() {
+        return null;
+    }
+
+    @Override
+    public CachedServerIcon loadServerIcon(File file) throws IllegalArgumentException, Exception {
+        return null;
+    }
+
+    @Override
+    public CachedServerIcon loadServerIcon(BufferedImage image) throws IllegalArgumentException, Exception {
+        return null;
+    }
+
+    @Override
+    public void setIdleTimeout(int threshold) {
+
+    }
+
+    @Override
+    public int getIdleTimeout() {
+        return 0;
+    }
+
+    @Override
+    public ChunkGenerator.ChunkData createChunkData(World world) {
+        return null;
+    }
+
+    @Override
+    public BossBar createBossBar(String title, BarColor color, BarStyle style, BarFlag... flags) {
+        return null;
+    }
+
+    @Override
+    public Entity getEntity(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public Advancement getAdvancement(NamespacedKey key) {
+        return null;
+    }
+
+    @Override
+    public Iterator<Advancement> advancementIterator() {
+        return null;
+    }
+
+    @Override
+    public UnsafeValues getUnsafe() {
+        return null;
+    }
+
+    @Override
+    public void sendPluginMessage(Plugin source, String channel, byte[] message) {
+
+    }
+
+    @Override
+    public Set<String> getListeningPluginChannels() {
+        return null;
+    }
+
+}
