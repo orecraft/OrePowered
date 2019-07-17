e91dbee9b95326306950a4e0e21402e150b24d60
hempflower <1993996310@qq.com>
2019-07-17 17:31:11 +0800

    移除自己的实现，因为完整实现一个Bukkit API 实在是太难了

--- src/main/java/cn/orecraft/orepowered/OrePowered.java
+++ src/main/java/cn/orecraft/orepowered/OrePowered.java
@@ -8,22 +8,12 @@
 
 package cn.orecraft.orepowered;
 
-import cn.orecraft.orepowered.util.LogRedirectHandler;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bukkit.Bukkit;
-
-import java.io.PrintStream;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 
 @SideOnly(Side.SERVER)
 @Mod(modid = "orepowered", version = "1.12.2")
@@ -32,26 +22,10 @@ public class OrePowered {
 
     @Mod.EventHandler
     public void serverStarting(FMLServerAboutToStartEvent event) {
-
         logger.info("Loading OrePowered");
         //Redirect logging
-        java.util.logging.Logger go = java.util.logging.Logger.getLogger("");
-        go.setUseParentHandlers(false);
 
-        for (Handler handler : go.getHandlers()) {
-            go.removeHandler(handler);
-        }
-        go.addHandler(new LogRedirectHandler());
-        final org.apache.logging.log4j.core.Logger logger = ((org.apache.logging.log4j.core.Logger) LogManager.getRootLogger());
-        for (org.apache.logging.log4j.core.Appender appender : logger.getAppenders().values()) {
-            if (appender instanceof org.apache.logging.log4j.core.appender.ConsoleAppender) {
-                logger.removeAppender(appender);
-            }
-        }
 
-        //Run test code.
-        Bukkit.setServer(new OreServer(event.getServer()));
-        //Bukkit.getServer().getViewDistance();
     }
 
 
